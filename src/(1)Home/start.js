d0dd09d535cf0fb52f99ce3269987653d8113d03
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-10-04 21:11:27 -0500

    primer avance

---
+++ src/(1)Home/start.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './start.css';
+
+const start = () => (
+    <div>
+    <button className="startBtn">Comenzar</button>
+    </div>
+);
+
+export default start;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo_uaomon from './Images/Logo_uaomon.svg';
 import './App.css';
+import start from './(1)Home/start';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">    
+      </header>
+      <header className="Button">
+      <start></start>
       </header>
     </div>
   );
