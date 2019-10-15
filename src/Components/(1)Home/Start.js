6636c82ba0fdb380b229bda395c7059749b2f682
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-10-15 18:40:59 -0500

    responsive pantalla 1

--- src/Components/(1)Home/Start.js
+++ src/Components/(1)Home/Start.js
@@ -1,15 +1,20 @@
+//Dependencies
 import React from 'react';
+
+//Style
 import './Start.css';
+
+//Assets
 import Logo from "../Images/uaomon-logo.png";
 import { Link } from 'react-router-dom';
 
 const Start = () => (
     <div className="home-container">
         <div>
-        <img src={Logo} />
+            <img src={Logo} />
         </div>
         <div>
-        <Link to='/seleccion'><button className="btn-style-home">Comenzar</button></Link>
+            <Link to='/seleccion'><button className="btn-style-home">Comenzar</button></Link>
         </div>
     </div>
 )
